4bb1b5aabb46c1ad63deef6ed00497a328cd8362
nnazis <nazira08.com@gmail.com>
2026-05-17 21:29:20 +0500

    Delete src/project/ReportGenerator.java

--- src/project/ReportGenerator.java
+++
@@ -1,8 +0,0 @@
-package project;
-
-public interface ReportGenerator {
-	Report generateStudentPerformanceReport(List<Student> students);
-	Report generateCourseStatisticsReport(Course course);
-	Report generateTeacherStatisticsReport(Teacher teacher);
-	Report generateResearchStatisticsReport(List<Researcher> researchers);
-}
